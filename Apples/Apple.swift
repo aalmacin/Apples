c96e9bb0856fbe4c4ed7d852b598685f47cd46a7
AJ Almacin <aldrinjerome19@gmail.com>
2015-04-04 18:35:54 -0400

    Added initial files

---
+++ Apples/Apple.swift
@@ -0,0 +1,12 @@
+//
+//  Apple.swift
+//  Apples
+//
+//  Created by Aldrin Jerome Almacin on 2015-04-04.
+//  Copyright (c) 2015 Almacin. All rights reserved.
+//
+import SpriteKit
+
+class Apple: SKSpriteNode {
+   
+}
---
+++ Apples/Boy.swift
@@ -0,0 +1,12 @@
+//
+//  Boy.swift
+//  Apples
+//
+//  Created by Aldrin Jerome Almacin on 2015-04-04.
+//  Copyright (c) 2015 Almacin. All rights reserved.
+//
+import SpriteKit
+
+class Boy: SKNode {
+   
+}
